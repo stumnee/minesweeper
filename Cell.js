6851be23045692be9f626b83dac8b150036f6abb
Saran Tumnee <stumnee@gmail.com>
2020-02-26 17:09:13 -0800

    added right click flag and game over/completion

--- Cell.js
+++ Cell.js
@@ -1,22 +1,24 @@
 class Cell {
-    static w = 20
+    static w = 50
+    static top = 50
     constructor(x, y, mine = false) {
         this.x = x;
         this.y = y;
         this.revealed = false;
+        this.flagged = false;
         this.mine = mine;
         this.nMineCount = 0;
     }
-    get centerX() {
-        return this.x + Cell.w * 0.5;
-    }
-    get centerY() {
-        return this.y + Cell.w * 0.5;
-    }
+
     reveal() {
         this.revealed = true;
     }
     show() {
+        let y = this.y + Cell.top
+        let x = this.x
+        let w = Cell.w
+        let cX = x + w * .5
+        let cY = y + w * .5
         stroke(0);
 
         if (!this.revealed) {
@@ -24,19 +26,31 @@ class Cell {
         } else {
             fill(200)
         }
-        rect(this.x, this.y, Cell.w, Cell.w)
-        if (!this.revealed) {
-            return;
-        }
-        if (this.mine) {
-            fill(155);
-            circle(this.centerX, this.centerY, Cell.w * 0.5);
-        } else {
-            textAlign(CENTER, CENTER);
+        rect(x, y, w, w)
+        if (this.flagged) {
             stroke(60);
-            fill(100);
-            if (this.nMineCount > 0) {
-                text(this.nMineCount, this.x + 2, this.y, Cell.w, Cell.w)
+            fill(255, 0, 0);
+            triangle(x + w/2, y + w/4, x + w/4*3, y + w/8 * 3, x + w/2, y + w/2)
+            line(x + w/2, y + w/2, x + w/2, y + w/4 * 3)
+            line(x + w/4 - 2, y + w/4*3, x + w/4*3 + 2, y + w/4*3)
+        }
+
+        if (this.revealed) {
+            if (this.mine) {
+                fill(255, 0, 0);
+                circle(cX, cY, w / 4);
+                line(cX,y + w/4, cX, y + w*3/4)
+                line(x + w / 4, cY, x + w*3/4 , cY)
+                line(x + w / 4, y + w/4, x + w*3/4, y + w*3/4)
+                line(x + w / 4, y + w*3/4, x + w*3/4, y + w/4)
+            } else {
+                textAlign(CENTER, CENTER);
+                stroke(60);
+                fill(100);
+                if (this.nMineCount > 0) {
+                    textSize(24)
+                    text(this.nMineCount, x + 2, y, w, w)
+                }
             }
         }
 
--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 const grid = [];
-const mines = 20;
+let mines = 20;
+let gameOver = false;
+let minesDeployed = 0;
 const maxX = 10;
 const maxY = 10;
 const totalCells = maxY * maxX;
@@ -15,7 +17,9 @@ function xyToIdx(x, y, divisor = 1) {
     return parseInt(y / divisor) *  maxY + parseInt(x / divisor);
 }
 
-function xyToCell(x, y) {
+function xyToCell(x, y, divisor = 1) {
+    x /= divisor
+    y /= divisor
     if (x >= 0 && x < maxX && y >= 0 && y < maxY) {
         return grid[xyToIdx(x, y)]
     }
@@ -23,18 +27,18 @@ function xyToCell(x, y) {
 }
 
 function setup() {
-  createCanvas(200, 200);
+    createCanvas(maxX * Cell.w, maxY * Cell.w + Cell.top);
 
-  for (let i = 0; i < totalCells; i++) {
+    for (let i = 0; i < totalCells; i++) {
       let xy = idxToXY(i, Cell.w);
       grid.push(new Cell(xy.x, xy.y));
-  }
+    }
 
-  // Place mines
-  let i = 0;
-  let pct = 1 / totalCells;
-  let minesDeployed = 0;
-  while (minesDeployed < mines) {
+    // Place mines
+    let i = 0;
+    let pct = 1 / totalCells;
+
+    while (minesDeployed < mines) {
       if (!grid[i].mine && Math.random() < pct) {
           grid[i].mine = true;
           minesDeployed++;
@@ -43,25 +47,46 @@ function setup() {
       if (i === totalCells) {
           i = 0;
       }
-  }
+    }
 
-  // Calculate neighbor mine counts
-  grid.forEach((cell, idx) => {
+    // Calculate neighbor mine counts
+    grid.forEach((cell, idx) => {
       let xy = idxToXY(idx)
       cell.nMineCount = neighborsOffset.filter(offset =>
                                                     xyToCell(xy.x + offset[0],xy.y + offset[1]).mine)
                                         .length
-  })
+    })
 }
 
 function mouseReleased() {
-    reveal(xyToIdx(mouseX, mouseY, Cell.w))
+    let cell = xyToCell(mouseX, mouseY - Cell.top, Cell.w)
+
+    if (cell.x === undefined) //outside
+        return
+
+    if (mouseButton === RIGHT) {
+        if (!cell.revealed) {
+
+            cell.flagged = !cell.flagged
+            mines += cell.flagged ? -1 : 1
+        }
+        return
+
+    }
+    if (!cell.flagged) {
+        if (cell.mine) {
+            gameOver = true
+            grid.forEach(cell => cell.revealed = true)
+        } else {
+            reveal(xyToIdx(mouseX, mouseY - Cell.top, Cell.w))
+        }
+    }
 }
 
 // reveal non mine cells in Flood Fill pattern
 function reveal(idx) {
     grid[idx].reveal()
-    if (grid[idx].nMineCount > 0 || grid[idx].mine) {
+    if (grid[idx].nMineCount > 0 || grid[idx].mine || grid[idx].flagged) {
         return;
     }
     let xy = idxToXY(idx)
@@ -75,13 +100,30 @@ function reveal(idx) {
 }
 
 function draw() {
-  background(0);
+    background(0);
 
-  grid.forEach(cell=>cell.show())
+    grid.forEach(cell=>cell.show())
 
+    fill(255);
+    textAlign(LEFT, CENTER);
+    textSize(24)
+    text("Mines Left: " + mines, 0, 30)
+
+    if (gameOver) {
+        fill(255, 0, 0);
+        textAlign(RIGHT, CENTER);
+        text("Game Over!", maxX * Cell.w, 30)
+    }
+
+    if (mines === 0 && maxX * maxY === minesDeployed + grid.filter(cell=>cell.revealed).length) {
+        fill(0, 255, 0);
+        textAlign(RIGHT, CENTER);
+        text("Good Job!", maxX * Cell.w, 30)
+    }
 }
 
 //disable right mouse click
 window.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 }, false);
+
